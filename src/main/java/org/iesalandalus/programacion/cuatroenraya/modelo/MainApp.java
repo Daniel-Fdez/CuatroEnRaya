cd63a5c0980127aa979c51a80be3311043dfbd7f
DanielFernandezDiez <pure_code@outlook.com>
2021-01-18 23:37:38 +0100

    Creación de la clase MainApp

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/MainApp.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class MainApp {
+	private Tablero tablero;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tablero tablero = new Tablero();
+		System.out.println(tablero.estaVacio());
+	}
+
+}
