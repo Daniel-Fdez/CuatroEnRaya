a70de7780db8825ca8ca53e21f4f1316138bc3a9
DanielFernandezDiez <pure_code@outlook.com>
2021-01-18 23:37:10 +0100

    Creación de la clase Jugador

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -4,15 +4,18 @@ public class Jugador {
 	private String nombre;
 	private Ficha colorFichas;
 	
+	// Constructor para establecer el nombre del jugador y el color de las fichas
 	public Jugador(String nombre, Ficha colorFichas) {
 		setNombre(nombre);
 		setColorFichas(colorFichas);
 	}
 	
+	// Devuelve el nombre del jugador
 	public String getNombre() {
 		return nombre;
 	}
 	
+	// Establece el nombre del jugador y comprueba errores
 	private void setNombre(String nombre) {
 		if(nombre == null) {
 			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
@@ -23,10 +26,12 @@ public class Jugador {
 		this.nombre = nombre;
 	}
 	
+	// Devuelve el color de las fichas
 	public Ficha getColorFichas() {
 		return colorFichas;
 	}
 	
+	// Establece el color de las fichas y comprueba que no se pase un valor nulo
 	private void setColorFichas(Ficha colorFichas) {
 		if(colorFichas == null) {
 			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
@@ -34,6 +39,7 @@ public class Jugador {
 		this.colorFichas = colorFichas;
 	}
 
+	// Método toString para mostrar el nombre del jugador y el color de las fichas
 	@Override
 	public String toString() {
 		return nombre + " (" + colorFichas + ")";
