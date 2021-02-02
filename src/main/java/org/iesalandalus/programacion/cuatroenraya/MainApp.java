be87b3a6c17596f66c942a17ba12aac4e2bea042
DanielFernandezDiez <pure_code@outlook.com>
2021-02-02 20:56:11 +0100

    Creada la clase MainApp

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
@@ -0,0 +1,25 @@
+package org.iesalandalus.programacion.cuatroenraya;
+
+import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
+import org.iesalandalus.programacion.cuatroenraya.vista.Consola;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Ficha colorFicha;
+		Jugador primerJugador = Consola.leerJugador();
+		
+		if(primerJugador.getColorFichas() == Ficha.AZUL) {
+			colorFicha = Ficha.VERDE;
+		} else {
+			colorFicha = Ficha.AZUL;
+		}
+		
+		Jugador segundoJugador = Consola.leerJugador(colorFicha);
+		CuatroEnRaya cuatroEnRaya = new CuatroEnRaya(primerJugador, segundoJugador);
+		cuatroEnRaya.jugar();
+
+	}
+
+}
